0322992839cf10f487c1541dfb714ca27521921f
anu-oluwapo <abolarin.aanu@gmail.com>
2021-04-23 23:06:31 +0100

    Uploaded lessons to mongodb

---
+++ lessonss.js
@@ -0,0 +1,208 @@
+let lessonsArray = [
+
+    {
+        "id": 1,
+        "subject": "Math",
+        "location": "London" ,
+        "price" : 300,
+        "availablespace": 5,
+        "rating" : 2,
+        
+    },
+    
+    {
+        "id": 2,
+        "subject": "English",
+        "location": "South Hampton" ,
+        "price" : 100,
+        "availablespace": 7,
+        "rating" : 2,
+        
+    },
+    
+    {
+        "id": 3,
+        "subject": "Music",
+        "location": "Germany" ,
+        "price" : 200,
+        
+        "availablespace": 9,
+        "rating" : 2,
+        
+    },
+    
+    {
+        "id": 4,
+        "subject": "Geography",
+        "location": "Hendon" ,
+        "price" : 250,
+        "availablespace": 6,
+        "rating" : 2,
+        
+    },
+    
+    {
+        "id": 5,
+        "subject": "History",
+        "location": "Liverpool" ,
+        "price" : 700,
+        "availablespace": 12,
+        "rating" : 2,
+        
+    },
+    
+    {
+        "id": 6,
+        "subject": "Theology",
+        "location": "Oxford" ,
+        "price" : 120,
+        "availablespace": 10,
+        "rating" : 2,
+        
+    },
+    
+    {
+        "id": 7,
+        "subject": "Social Studies",
+        "location": "Edinburgh" ,
+        "price" : 260,
+        "availablespace": 24,
+        "rating" : 2,
+        
+    },
+    
+    {
+        "id": 8,
+        "subject": "Health Education",
+        "location": "Nottinham" ,
+        "price" : 900,
+        "availablespace": 15,
+        "rating" : 2,
+        
+    },
+    
+    {
+        "id": 9,
+        "subject": "Business",
+        "location": "Manchester" ,
+        "price" : 310,
+        "availablespace": 16,
+        "rating" : 2,
+        
+    },
+    
+    {
+        "id": 10,
+        "subject": "Info-Technology",
+        "location": "Brighton" ,
+        "price" : 525,
+        "availablespace": 21,
+        "rating" : 2,
+        
+    },
+    ];
+
+
+let lessonsArray = [
+
+    {
+        id: 1,
+        "subject": "Math",
+        "location": "London" ,
+        "price" : 300,
+        "availablespace": 5,
+        "rating" : 2,
+        
+    },
+    
+    {
+        id: 2,
+        "subject": "English",
+        "location": "South Hampton" ,
+        "price" : 100,
+        "availablespace": 7,
+        "rating" : 2,
+        
+    },
+    
+    {
+        id: 3,
+        "subject": "Music",
+        "location": "Germany" ,
+        "price" : 200,
+        
+        "availablespace": 9,
+        "rating" : 2,
+        
+    },
+    
+    {
+        id: 4,
+        "subject": "Geography",
+        "location": "Hendon" ,
+        "price" : 250,
+        "availablespace": 6,
+        "rating" : 2,
+        
+    },
+    
+    {
+        id: 5,
+        "subject": "History",
+        "location": "Liverpool" ,
+        "price" : 700,
+        "availablespace": 12,
+        "rating" : 2,
+        
+    },
+    
+    {
+        id: 6,
+        "subject": "Theology",
+        "location": "Oxford" ,
+        "price" : 120,
+        "availablespace": 10,
+        "rating" : 2,
+        
+    },
+    
+    {
+        id: 7,
+        "subject": "Social Studies",
+        "location": "Edinburgh" ,
+        "price" : 260,
+        "availablespace": 24,
+        "rating" : 2,
+        
+    },
+    
+    {
+        id: 8,
+        "subject": "Health Education",
+        "location": "Nottinham" ,
+        "price" : 900,
+        "availablespace": 15,
+        "rating" : 2,
+        
+    },
+    
+    {
+        id: 9,
+        "subject": "Business",
+        "location": "Manchester" ,
+        "price" : 310,
+        "availablespace": 16,
+        "rating" : 2,
+        
+    },
+    
+    {
+        id: 10,
+        "subject": "Info-Technology",
+        "location": "Brighton" ,
+        "price" : 525,
+        "availablespace": 21,
+        "rating" : 2,
+        
+    },
+    ];
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app  = express();
 
+app.use(express.json())
+
 const http = require('http');
 const port = 2000;
 const server = http.createServer(app);
@@ -15,17 +17,80 @@ const courses = [
 
 const user = [{'image': 'user.png', 'name': 'Anuoluwapo', 'email': 'EE5043@live.mdx.ac.uk', 'password': 'mypassword'}];
 
-app.get('/lesson', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(JSON.stringify(courses));
-});
 
-app.get('/checkout', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(JSON.stringify(user));
+// Connect to MongoDb Atlas
+const MongoClient = require('mongodb').MongoClient;
+ let db;
+  MongoClient.connect('mongodb+srv://anu:kntjf04mwl06@cluster0.rpda3.mongodb.net/coursework2?retryWrites=true&w=majority', (err, client) => {
+      db = client.db('coursework2');
+ })
+
+ // Get the collection Name 
+ app.param('collectionName', (req, res, next, collectionName)=>{
+     req.collection = db.collection(collectionName)
+     return next();
+ })
+
+ // Display Message for the root path to show the API is working
+ app.get('/', (req, res) =>{
+     res.send('Api is working, Please select a collection, eg collection/messages');
+ })
+
+ //Retrieve all the objects from a collection within MongoDB
+ app.get('collection/:collectionName', (req, res) =>{
+     req.collection.find({}).toArray((e, results) => {
+         if (e) return next(e)
+         res.send(results)
+     })
+ })
+
+
+//Add an object to Mongodb
+app.post('/collection/:collectionName', (req,res,next) => {
+ try{ req.collection.insert(req.body, (e, results) => {
+      if (e) return next (e)
+      res.send(results.ops)
+  })
+} catch(error){
+  res.status(500).json({message:"Error Message"})
+  console.log(error);
+}
 })
+
+//  // Update an object by ID
+//  app.put('/collection/:collectionName/:id', (req, res, next) => {
+//     req.collection.update(
+//         { _id: new ObjectID(req.params.id)},
+//         {$set: req.body},
+//         {safe: true, multi: false},
+//         (e, result) => {
+//             if (e) return next (e)
+//             res.send ((result.result.n === 1) ? { msg: 'success'} : {msg: 'error'})
+//         }
+//     )
+//  })
+
+// // Delete An object By ID
+// app.delete('/collection/:collectionName/:id', (req, res, next) =>{
+//     req.collection.deleteOne(
+//         {_id: ObjectID(req.params.id)},
+//         (e, result) => {
+//             if (e) return next(e)
+//             res.send(result.result.n === 1) ? {msg: 'success'} : {msg : 'error'}
+//         })
+// })
+
+// app.get('/lesson', (req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end(JSON.stringify(courses));
+// });
+
+// app.get('/checkout', (req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end(JSON.stringify(user));
+// });
 
 server.listen(port);
 console.log('Sever Is Now Running On Port 2000');
